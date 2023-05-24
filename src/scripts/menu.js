d67483e6ddc5dd3a48d539d6869e17558b4b20ef
Olena Yastrebtsova <uaalena202@gmail.com>
2023-05-24 09:13:29 +0300

    new files

---
+++ src/scripts/menu.js
@@ -0,0 +1,12 @@
+﻿let burger  = document.querySelector(".hamburger");
+let menu = document.querySelector(".nav-links");
+burger.addEventListener("click", () => {
+    console.log("n")
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+        console.log("n")
+    } else {
+        menu.classList.add('hidden');
+        console.log("n")
+    }
+});
